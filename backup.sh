01587aa42a339f4ce694a216af90604a1235cc16
Takamatsu Makoto <tkmtmkt@gmail.com>
2016-04-20 20:46:46 +0900

    fix

--- backup.sh
+++ backup.sh
@@ -2,14 +2,14 @@
 REPOS_DIR=~/work/yum.repos
 BACKUP_DIR=$REPOS_DIR/backup
 
-やりかけ
-
 while read REPOS
 do
-  tar -g $BACKUP_DIR/backup.snar -C $REPOS_DIR -zcf $BACKUP_DIR/backup-hoge-`date +%Y%m%d`.tar.gz $REPOS
+  NAME=$(echo $REPOS | sed -e 's/\//_/g')
+  echo ">>> backup $NAME"
+  tar -g $BACKUP_DIR/backup.snar -C $REPOS_DIR -zcf $BACKUP_DIR/backup-$NAME-`date +%Y%m%d`.tar.gz $REPOS
 done <<EOS
-pub/Linux/CentOS/7/os/x86_64
-pub/Linux/Fedora/epel/7/x86_64
-pub/Linux/CentOS/7/updates/x86_64
-pub/Linux/CentOS/7/extras/x86_64
+pub/Linux/CentOS/7/os
+pub/Linux/Fedora/epel/7
+pub/Linux/CentOS/7/updates
+pub/Linux/CentOS/7/extras
 EOS
